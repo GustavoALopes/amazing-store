9f09baa698b8dbf5315832eb515d16e5cc519e71
lgalopes <lgalopes20@gmail.com>
2023-09-05 08:49:48 -0300

    FEATURE - base commit

---
+++ src/main/java/com/developerjorney/AmazingStoreMain.java
@@ -0,0 +1,12 @@
+package com.developerjorney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazingStoreMain {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(AmazingStoreMain.class, args);
+    }
+}
