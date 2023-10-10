f3521807c849b7353452d6d8c23244fcaf495c1a
lgalopes <lgalopes20@gmail.com>
2023-10-10 09:14:40 -0300

    FEATURE - adding swagger

--- src/main/java/com/developerjorney/application/client/dtos/input/GetClientReportInput.java
+++ src/main/java/com/developerjorney/application/client/dtos/input/GetClientReportInput.java
@@ -1,4 +1,25 @@
 package com.developerjorney.application.client.dtos.input;
 
-public record GetClientReportInput(String name, String lastName, RangeDateInput dates) {
-}
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
+
+public record GetClientReportInput(
+
+        @Size(
+            max = 100,
+            min = 1
+        )
+        @Schema(
+            description = "The client name",
+            example = "Benjamin"
+        )
+        String name,
+
+        @Schema(
+                description = "The client last name",
+                example = "Oliver"
+        )
+        String lastName,
+
+        RangeDateInput dates
+) {}
--- src/main/java/com/developerjorney/application/client/dtos/input/RangeDateInput.java
+++ src/main/java/com/developerjorney/application/client/dtos/input/RangeDateInput.java
@@ -1,4 +1,17 @@
 package com.developerjorney.application.client.dtos.input;
 
-public record RangeDateInput(String initDate, String finalDate) {
-}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record RangeDateInput(
+        @Schema(
+                description = "The initial Date in ISO8601",
+                example = "2000-12-01"
+        )
+        String initDate,
+
+        @Schema(
+                description = "The final Date in ISO8601",
+                example = "2001-12-01"
+        )
+        String finalDate
+) {}
